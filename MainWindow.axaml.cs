409faf7e078affaa694fc49ebea9b8869502e86e
hugo <aesthiadev@gmail.com>
2026-06-12 23:27:56 +0200

    Added Statistics Features

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -11,6 +11,7 @@ namespace MyFirstApp;
 public partial class MainWindow : Window
 {
     private int clicks = 0;
+    private int totalClicks = 0;
     private int clickPerClick = 1;
     private int upgrades = 0;
     private int upgradeCost = 20;
@@ -18,12 +19,16 @@ public partial class MainWindow : Window
     private int autoClickers = 0;
     private int autoClickersCost = 100;
 
+    private TimeSpan playTime = TimeSpan.Zero; // Timespan zegar
+    private string formattedTime = "00:00:00"; // Format 
+
     private string path = $@"{AppDomain.CurrentDomain.BaseDirectory}\save.txt";
 
     private DispatcherTimer autoClickerTimer = new DispatcherTimer();
 
     public MainWindow()
     {
+
         InitializeComponent();
 
         autoClickerTimer.Interval = TimeSpan.FromSeconds(1); // Ustawienie tick
@@ -35,11 +40,21 @@ public partial class MainWindow : Window
     private void AutoClickerTick(object? sender, EventArgs e)
     {
         clicks += autoClickers * prestige;
+        totalClicks += autoClickers * prestige;
+
+        playTime = playTime.Add(TimeSpan.FromSeconds(1)); // Dodawanie sekund co 1s
+        formattedTime = playTime.ToString(@"hh\:mm\:ss"); // Formatowanie czasu
 
         var prestigeProgress = this.FindControl<ProgressBar>("PrestigeProgress");
         var scoreText = this.FindControl<TextBlock>("ScoreText");
+        var totalText = this.FindControl<TextBlock>("TotalClicks");
+        var playTimeText = this.FindControl<TextBlock>("PlayTime");
+
+        totalText!.Text = $"Total Clicks: {totalClicks}";
         scoreText!.Text = $"{clicks}";
         prestigeProgress!.Value = clicks;
+
+        playTimeText!.Text = $"Play Time: {formattedTime}";
     }
 
     private void Click_Button(object? sender, RoutedEventArgs e)
@@ -47,9 +62,13 @@ public partial class MainWindow : Window
         var clickValue = this.FindControl<TextBlock>("ScoreText");
         var infoText = this.FindControl<TextBlock>("InfoText");
         var prestigeProgress = this.FindControl<ProgressBar>("PrestigeProgress");
+        var totalText = this.FindControl<TextBlock>("TotalClicks");
+        
 
         clicks = clicks + clickPerClick * prestige;
+        totalClicks = totalClicks + clickPerClick * prestige;
 
+        totalText!.Text = $"Total Clicks: {totalClicks}";
         clickValue!.Text = $"{clicks}";
         prestigeProgress!.Value = clicks;
     }
@@ -83,22 +102,33 @@ public partial class MainWindow : Window
         var prestigeProgress = this.FindControl<ProgressBar>("PrestigeProgress");
         var clickerCost = this.FindControl<TextBlock>("AutoClickerCostText");
         var text = this.FindControl<TextBlock>("AutoClickerCountText");
+        var prestigeStat = this.FindControl<TextBlock>("Prestiges");
+        var totalText = this.FindControl<TextBlock>("TotalClicks");
+        var playTimeText = this.FindControl<TextBlock>("PlayTime");
 
         clicks = 0;
+        totalClicks = 0;
         clickPerClick = 1;
         upgrades = 0;
         upgradeCost = 20;
         prestige = 1;
         autoClickers = 0;
         autoClickersCost = 100;
+        playTime = TimeSpan.Zero;
 
         prestigeProgress!.Value = clicks;
+        prestigeStat!.Text = $"Prestiges: {prestige}";
         prestigeText!.Text = $"Multiplier: x{prestige}";
         upgradeText!.Text = $"Upgrade Cost: {upgradeCost}";
         infoText!.Text = $"Clicks per click: {clickPerClick}";
         clickValue!.Text = $"{clicks}";
         clickerCost!.Text = $"Cost: {autoClickersCost}";
         text!.Text = $"Owned: {autoClickers}";
+        totalText!.Text = $"Total Clicks: {totalClicks}";
+
+        formattedTime = playTime.ToString(@"hh\:mm\:ss");
+
+        playTimeText!.Text = $"Play Time: {formattedTime}";
     }
      
     private void AutoClicker_Button(object? sender, RoutedEventArgs e)
@@ -128,6 +158,7 @@ public partial class MainWindow : Window
         var prestigeProgress = this.FindControl<ProgressBar>("PrestigeProgress");
         var clickerCost = this.FindControl<TextBlock>("AutoClickerCostText");
         var text = this.FindControl<TextBlock>("AutoClickerCountText");
+        var prestigeStat = this.FindControl<TextBlock>("Prestiges");
 
         if (clicks >= prestigeProgress!.Maximum)
         {
@@ -139,6 +170,7 @@ public partial class MainWindow : Window
             autoClickersCost = 100;
             prestige++;
 
+            prestigeStat!.Text = $"Prestiges: {prestige}";
             prestigeProgress!.Value = clicks;
             prestigeProgress!.Maximum = prestigeProgress.Maximum * 2;
             prestigeText!.Text = $"Multiplier: x{prestige}";
@@ -153,7 +185,7 @@ public partial class MainWindow : Window
     private void Save_Button(object? sender, RoutedEventArgs e)
     {
         var prestigeProgress = this.FindControl<ProgressBar>("PrestigeProgress");
-        string createText = $"clicks={clicks}\nclickPerClick={clickPerClick}\nupgrades={upgrades}\nupgradeCost={upgradeCost}\nprestige={prestige}\nautoClicker={autoClickers}\nautoClickersCost={autoClickersCost}\nprestigeMaxValue={prestigeProgress!.Maximum}";
+        string createText = $"clicks={clicks}\nclickPerClick={clickPerClick}\nupgrades={upgrades}\nupgradeCost={upgradeCost}\nprestige={prestige}\nautoClicker={autoClickers}\nautoClickersCost={autoClickersCost}\nprestigeMaxValue={prestigeProgress!.Maximum}\nplayTime={playTime.TotalSeconds}\ntotalClicks={totalClicks}";
         File.WriteAllText(path, createText);
     }
 
@@ -171,6 +203,8 @@ public partial class MainWindow : Window
             string[] s6 = readText[5].Split("="); // autoClickers
             string[] s7 = readText[6].Split("="); // autoClickersCost
             string[] s8 = readText[7].Split("="); // prestigeProgressMaximum
+            string[] s9 = readText[8].Split("="); // playTime
+            string[] s10 = readText[9].Split("="); // totalClicks
 
             clicks = int.Parse(s[1]);
             clickPerClick = int.Parse(s2[1]);
@@ -179,6 +213,8 @@ public partial class MainWindow : Window
             prestige = int.Parse(s5[1]);
             autoClickers = int.Parse(s6[1]);
             autoClickersCost = int.Parse(s7[1]);
+            playTime = TimeSpan.FromSeconds(int.Parse(s9[1]));
+            totalClicks = int.Parse(s10[1]);
 
             var clickValue = this.FindControl<TextBlock>("ScoreText");
             var infoText = this.FindControl<TextBlock>("InfoText");
@@ -187,7 +223,12 @@ public partial class MainWindow : Window
             var clickerCost = this.FindControl<TextBlock>("AutoClickerCostText");
             var autoClickerCount = this.FindControl<TextBlock>("AutoClickerCountText");
             var upgradeText = this.FindControl<TextBlock>("UpgradeText");
+            var playTimeText = this.FindControl<TextBlock>("PlayTime");
+            var prestigeStat = this.FindControl<TextBlock>("Prestiges");
+            var totalClicksText = this.FindControl<TextBlock>("TotalClicks");
 
+            prestigeStat!.Text = $"Prestiges: {prestige}";
+            totalClicksText!.Text = $"Total Clicks: {totalClicks}";
             clickValue!.Text = $"{clicks}";
             infoText!.Text = $"Clicks per click: {clickPerClick}";
             upgradeText!.Text = $"Upgrade Cost: {upgradeCost}";
@@ -196,6 +237,9 @@ public partial class MainWindow : Window
             prestigeProgress!.Maximum = int.Parse(s8[1]);
             clickerCost!.Text = $"Cost: {autoClickersCost}";
             autoClickerCount!.Text = $"Owned: {autoClickers}";
+
+            formattedTime = playTime.ToString(@"hh\:mm\:ss");
+            playTimeText!.Text = $"Play Time: {formattedTime}";
         }
     }
 }
